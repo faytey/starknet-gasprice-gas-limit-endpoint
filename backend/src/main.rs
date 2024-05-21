3ca815e8298c63ff4afcff2421901b688c2c64bb
sprtd <sagesdn@gmail.com>
2024-05-21 18:26:00 +0100

    chore: update dockerfile

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -14,7 +14,7 @@ mod server;
 #[tokio::main]
 async fn main() {
     let routes_all: Router = Router::new().merge(server::routes_handler::routes());
-    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 5000));
+    let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("->> SERVER RUNNING ON {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes_all.into_make_service())
