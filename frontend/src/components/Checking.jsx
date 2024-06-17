82127b8c1001dcdc486a89f24861df852c35465d
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-06-17 23:37:22 +0100

    mobile modal fix

--- frontend/src/components/Checking.jsx
+++ frontend/src/components/Checking.jsx
@@ -59,7 +59,7 @@ function Checking({ onClose }) {
             width="24"
             height="24"
             viewBox="0 0 50 50"
-            className=" absolute cursor-pointer top-0 right-0 text-gray-950 hover:fill-red-800 hover:scale-110"
+            className=" absolute cursor-pointer md:top-0 md:right-0 -top-8 -right-8 text-gray-950 hover:fill-red-800 hover:scale-110"
             onClick={onClose}
           >
             <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
